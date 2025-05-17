b4f06035de704bd672251e9e1f3276eed77b4257
German Ormeño <ge.ormeno@duocuc.cl>
2025-05-17 10:40:06 -0400

    Delete cursos directory

--- cursos/src/main/java/com/gamje/cursos/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gamje.cursos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.gamje.cursos.model.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Integer> {
-}
